05812be9962a16c6d11ed2560223e5fc6c288c3a
Luke Epp <lucasfepp@gmail.com>
2016-06-20 16:31:41 -0700

    Semicolons are hard

--- public/send-email.php
+++ public/send-email.php
@@ -6,6 +6,6 @@ $email = getenv('EMAIL_ADDRESS');
 if ($request) {
   $subject = "Twilio recording received from " . $request['From'] . ". SID: " . $request['AccountSid'];
   $message = "New Twilio recording received.\r\nRecording URL: " . $request['RecordingUrl'] . "\r\nRecording Duration: " . $request['RecordingDuration'] . "\r\nEnding digit: " . $request['Digits'];
-  mail($email, $subject, $message)
+  mail($email, $subject, $message);
 }
 ?>

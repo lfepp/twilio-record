8ab1f192e9c5b1fb52bb7ac2ec90d9e30ec55af5
Luke Epp <lucasfepp@gmail.com>
2016-06-20 11:58:15 -0700

    Adding email logic

--- public/send-email.php
+++ public/send-email.php
@@ -1,3 +1,11 @@
 <?php
-$messages = json_decode(file_get_contents("php://input"));
+$request = json_decode(file_get_contents("php://input"));
+
+$email = getenv('EMAIL_ADDRESS');
+
+if ($request) {
+  $subject = "Twilio recording received from " . $request['From'] . ". SID: " . $request['AccountSid'];
+  $message = "New Twilio recording received.\r\nRecording URL: " . $request['RecordingUrl'] . "\r\nRecording Duration: " . $request['RecordingDuration'] . "\r\nEnding digit: " . $request['Digits'];
+  mail($email, $subject, $message)
+}
 ?>
